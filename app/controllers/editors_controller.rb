7a90a6554ef2d97a6940e1d24e81eaa36dc84192
Maolei Tang <tangmaolei@wifi-131-179-5-239.host.ucla.edu>
2016-11-02 19:30:00 -0700

    useless commit

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -11,16 +11,16 @@ class EditorsController < ApplicationController
   		next_id = 0
   	else
   		next_id = Editor.maximum(:id)+1;
- 	end
- 	permitted = params.require(:editor).permit(:user1)
- 	permitted[:path] = next_id.to_s
- 	@editor = Editor.new(permitted)
+   	end
+   	permitted = params.require(:editor).permit(:user1)
+   	permitted[:path] = next_id.to_s
+   	@editor = Editor.new(permitted)
 
- 	if @editor.save
- 		redirect_to edit_editor_path(@editor.id)
- 	else 
- 		render 'new'
- 	end
+   	if @editor.save
+   		redirect_to edit_editor_path(@editor.id)
+   	else 
+   		render 'new'
+   	end
   end 
 
   def edit
