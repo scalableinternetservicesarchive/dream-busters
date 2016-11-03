97abfe657036a1dc22125650c931a46dc8c35533
Maolei Tang <tangmaolei@wifi-131-179-5-239.host.ucla.edu>
2016-11-02 20:12:19 -0700

    useless

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -11,14 +11,11 @@ class EditorsController < ApplicationController
   		next_id = 0
   	else
   		next_id = Editor.maximum(:id)+1;
-<<<<<<< HEAD
-=======
  	end
 
  	permitted = params.require(:editor).permit(:user1)
  	permitted[:path] = next_id.to_s
  	@editor = Editor.new(permitted)
->>>>>>> 8a884d327f5c45e0e0fb358f83c6dc1dbbfaf426
 
    	if @editor.save
    		redirect_to edit_editor_path(@editor.id)
