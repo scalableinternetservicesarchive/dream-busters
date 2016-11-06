342dc9a303cfafeaa272fb8a899949f09a18fc06
haoxiangzhx <haoxiangzhx@gmail.com>
2016-11-05 21:13:30 -0700

    login and bootstrap

--- test/controllers/editors_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EditorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get editors_show_url
-    assert_response :success
-  end
-
-end
