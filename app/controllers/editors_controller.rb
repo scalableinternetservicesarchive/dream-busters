217e0fb488238cbf8cb2159d28d5968ce43f6e4f
Jiuru Shao <shaojiuru@ucla.edu>
2016-11-06 18:05:32 -0800

    aws try

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -1,4 +1,32 @@
 class EditorsController < ApplicationController
+
+  def new
+    @editor = Editor.new(params[:editor])
+  end
+
+  def create
+    if Editor.maximum(:id).nil?
+      next_id = 0
+    else
+      next_id = Editor.maximum(:id)+1;
+    end
+
+    permitted = params.require(:editor).permit(:user1)
+    permitted[:path] = next_id.to_s
+    @editor = Editor.new(permitted)
+
+    if @editor.save
+      Dir.mkdir "/tmp/#{@editor.id}"
+      system("git -C /tmp/#{@editor.id} init")
+      system("touch /tmp/#{@editor.id}/file.txt")
+      system("git -C /tmp/#{@editor.id} add /tmp/#{@editor.id}/*")
+      system("git -C /tmp/#{@editor.id} commit -m \"init repo\"")
+      redirect_to edit_editor_path(@editor.id)
+    else
+      render 'new'
+    end
+  end
+
   def show
   	@messages = Message.all
   	@default_content = Editor.default_content
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'editors#show'
+  root to: 'welcome#index'
 
+  get '/newPage', to: 'editors#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end
