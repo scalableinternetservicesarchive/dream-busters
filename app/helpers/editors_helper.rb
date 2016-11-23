dc0b389316886de9891e9bd893ac8fc268c6fe3b
Jiuru Shao <shaojiuru@ucla.edu>
2016-11-23 00:49:25 -0800

    server side caching

--- app/helpers/editors_helper.rb
+++ app/helpers/editors_helper.rb
@@ -1,2 +1,11 @@
 module EditorsHelper
+  # def cache_key_for_chatroom(editor)
+  #
+  #   logger.info "chatroom-#{editor.id}-#{editor.messages.count}"
+  #
+  #   "chatroom-#{editor.id}-#{editor.messages.count}"
+  # end
+  # def cache_key_for_chatroom(message)
+  #   "chatroom-#{message.id}"
+  # end
 end
