a7356faa8eeee0d691af92e991e4f9aac8f481f7
Maolei Tang <tangmaolei@tangmaoleideMacBook-Pro.local>
2016-11-29 00:49:35 -0800

    add editor sample in db and enter existed editor

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,72 @@
 
 Editor.create(
     filename:                 'editor01',
+    hashpath:                 '111111',
     created_at:               '2016-11-06 01:08:46.995656',
     updated_at:               '2016-11-06 01:09:46.995656'
 )
 
 Editor.create(
     filename:                 'editor02',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    hashpath:                 '111112',
+    created_at:               '2016-11-07 01:08:46.995656',
+    updated_at:               '2016-11-07 01:09:46.995656'
 )
 
 Editor.create(
     filename:                 'editor03',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    hashpath:                 '111113',
+    created_at:               '2016-11-08 01:08:46.995656',
+    updated_at:               '2016-11-08 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor04',
+    hashpath:                 '111114',
+    created_at:               '2016-11-09 01:08:46.995656',
+    updated_at:               '2016-11-09 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor05',
+    hashpath:                 '111115',
+    created_at:               '2016-11-10 01:08:46.995656',
+    updated_at:               '2016-11-10 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor06',
+    hashpath:                 '111116',
+    created_at:               '2016-11-11 01:08:46.995656',
+    updated_at:               '2016-11-11 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor07',
+    hashpath:                 '111117',
+    created_at:               '2016-11-12 01:08:46.995656',
+    updated_at:               '2016-11-12 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor08',
+    hashpath:                 '111118',
+    created_at:               '2016-11-13 01:08:46.995656',
+    updated_at:               '2016-11-13 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor09',
+    hashpath:                 '111119',
+    created_at:               '2016-11-14 01:08:46.995656',
+    updated_at:               '2016-11-14 01:09:46.995656'
+)
+
+Editor.create(
+    filename:                 'editor10',
+    hashpath:                 '111120',
+    created_at:               '2016-11-15 01:08:46.995656',
+    updated_at:               '2016-11-15 01:09:46.995656'
 )
 
 User.create(
@@ -30,8 +82,8 @@ User.create(
     password:                 'test01',
     password_confirmation:    'test01',
     sign_in_count:            '0',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    created_at:               '2016-11-15 01:08:46.995656',
+    updated_at:               '2016-11-15 01:09:46.995656'
 )
 
 User.create(
@@ -39,8 +91,8 @@ User.create(
     password:                 'test02',
     password_confirmation:    'test02',
     sign_in_count:            '0',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    created_at:               '2016-11-16 01:08:46.995656',
+    updated_at:               '2016-11-16 01:09:46.995656'
 )
 
 User.create(
@@ -48,8 +100,8 @@ User.create(
     password:                 'test03',
     password_confirmation:    'test03',
     sign_in_count:            '0',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    created_at:               '2016-11-12 01:08:46.995656',
+    updated_at:               '2016-11-12 01:09:46.995656'
 )
 
 User.create(
@@ -57,8 +109,8 @@ User.create(
     password:                 'test04',
     password_confirmation:    'test04',
     sign_in_count:            '0',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    created_at:               '2016-11-15 01:08:46.995656',
+    updated_at:               '2016-11-15 01:09:46.995656'
 )
 
 User.create(
@@ -66,6 +118,7 @@ User.create(
     password:                 'test05',
     password_confirmation:    'test05',
     sign_in_count:            '0',
-    created_at:               '2016-11-06 01:08:46.995656',
-    updated_at:               '2016-11-06 01:09:46.995656'
+    created_at:               '2016-11-16 01:08:46.995656',
+    updated_at:               '2016-11-16 01:09:46.995656'
 )
+
