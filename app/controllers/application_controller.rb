bb9cb7713a56f80c52780a399076a221824b3c42
Jiuru Shao <shaojiuru@ucla.edu>
2016-11-30 21:22:55 -0800

    add Jdenticon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-  	welcomes_path(current_user)
+  	welcomes_path
   end
 
 end
--- app/helpers/editors_helper.rb
+++ app/helpers/editors_helper.rb
@@ -1,2 +1,6 @@
 module EditorsHelper
+  require 'digest/sha1'
+  def hash(speaker)
+    Digest::SHA1.hexdigest speaker
+  end
 end
